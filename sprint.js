3e6622f685cdbf16b95a79198246a917fb4e7e00
Remigijus Viciulis <remigijus.viciulis@gmail.com>
2022-04-28 22:53:00 +0300

    updated files with active checkbox

--- sprint.js
+++ sprint.js
@@ -1,29 +1,45 @@
-const taskInput = document.querySelector(".task-input input");
+const taskInput = document.querySelector(".task-input input"),
 taskBox = document.querySelector(".task-box");
 let itemsList = JSON.parse(localStorage.getItem("item-list"));
 
-function showTodo(filter) {
+
+function showTodo() {
     let li = "";
     if (itemsList) {
         itemsList.forEach((item, id) => {
-            li += `<li class="task">
+            let isCompleted = item.status == "completed" ? "checked" : "";
+    li += `<li class="task">
         <label for="${id}">
-            <input type="checkbox" id="${id}">
-            <p>${item.name}</p>
+            <input onclick = "updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
+            <p class="${isCompleted}">${item.name}</p>
         </label>
         <div class="settings">
             <i onclick ="showMenu(this)" class="fa fa-bars"></i>
             <ul class="task-menu">
-                <li>Edit</li>
-                <li>Delete</li>
+                <li onclick="editTask(${id}, '${item.name}')">Edit</li>
+                <li onclick="deleteTask(${id})">Delete</li>
             </ul>
         </div>
     </li>`;
-        });
-    }
+    });
+}
     taskBox.innerHTML = li || `<span>You don't have any items here</span>`;
 }
 showTodo();
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+        itemsList[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        itemsList[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("item-list", JSON.stringify(itemsList));
+}
+
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
