5876c711b62c499b87da7b11e5a5f0cd802bee2a
Remigijus Viciulis <remigijus.viciulis@gmail.com>
2022-04-28 22:32:56 +0300

    Updated project files with create localStorage function

--- sprint.js
+++ sprint.js
@@ -1,9 +1,34 @@
 const taskInput = document.querySelector(".task-input input");
+taskBox = document.querySelector(".task-box");
+let itemsList = JSON.parse(localStorage.getItem("item-list"));
+
+function showTodo(filter) {
+    let li = "";
+    if (itemsList) {
+        itemsList.forEach((item, id) => {
+            li += `<li class="task">
+        <label for="${id}">
+            <input type="checkbox" id="${id}">
+            <p>${item.name}</p>
+        </label>
+        <div class="settings">
+            <i onclick ="showMenu(this)" class="fa fa-bars"></i>
+            <ul class="task-menu">
+                <li>Edit</li>
+                <li>Delete</li>
+            </ul>
+        </div>
+    </li>`;
+        });
+    }
+    taskBox.innerHTML = li || `<span>You don't have any items here</span>`;
+}
+showTodo();
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
     if (e.key == "Enter" && userTask) {
-        let itemsList = JSON.parse(localStorage.getItem("item-list"));
+
         if (!itemsList) {
             itemsList = [];
         }
@@ -11,5 +36,6 @@ taskInput.addEventListener("keyup", e => {
         let taskInfo = { name: userTask, status: "pending" };
         itemsList.push(taskInfo);
         localStorage.setItem("item-list", JSON.stringify(itemsList));
+        showTodo();
     }
 });
